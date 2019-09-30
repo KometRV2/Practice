899094487a02bf2c893c5bc36bda49eb2ba61a69
m-ohmori <makoto.wdp@icloud.com>
2019-09-30 23:58:23 +0900

    LookAt追加

---
+++ TestProject/Assets/Scripts/LookChara.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookChara : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_LookTarget;
+    
+    private Vector3 m_Dir;
+    void Update()
+    {
+        m_Dir = m_LookTarget.position - this.transform.position;
+        this.transform.forward = m_Dir;
+    }
+}
